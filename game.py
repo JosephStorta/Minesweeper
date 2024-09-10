db99c34ef358b9c5c1daf1c07cfa1e8c75820414
GreenExplosion08 <jas@storta.net>
2024-09-10 15:56:57 -0400

    Add lose state

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ class Game:
         self.__mine_number = mine_number
         self.__flagged_cells = 0
 
+        self.__lose = False
+
         self.__grid = Grid(grid_size, mine_number)
         self.__board_position = ((SCREEN_SIZE[0] - (self.__grid_size[0] * TILE_SIZE[0] * TILE_SCALE)) // 2, 100)
 
@@ -16,15 +18,21 @@ class Game:
         self.__font = font.Font(None, 64)
 
     def update(self, delta: int):
+        if self.__lose:
+            return
+
         self.__timer += delta / 1000
     
     def handleEvents(self, event):
+        if self.__lose:
+            return
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_position = event.pos
             cell_position = ((mouse_position[0] - self.__board_position[0]) // TILE_SIZE[0] // TILE_SCALE, (mouse_position[1] - self.__board_position[1]) // TILE_SIZE[1] // TILE_SCALE)
             
             if event.button == 1:
-                self.__grid.revealCell(cell_position[0], cell_position[1])
+                self.__lose = self.__grid.revealCell(cell_position[0], cell_position[1])
             
             if event.button == 3:
                 flagged = self.__grid.flagCell(cell_position[0], cell_position[1])
@@ -48,6 +56,9 @@ class Game:
         for row in self.__grid.getCells():
             x = self.__board_position[0]
             for cell in row:
+                if self.__lose and cell.getState() == 9:
+                    cell.setRevealed(True)
+
                 if cell.isRevealed():
                     sprite = TILE_SPRITES[cell.getState()]
                 else:
